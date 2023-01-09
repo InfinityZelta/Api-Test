703c2a6501cf6a1ebc87543b397a74fc5ccf7d2b
Infinity_Zelta <lunalmon0@gmail.com>
2023-01-09 03:49:02 -0600

    Agregar archivos de proyecto.

---
+++ Api-Test/Controllers/PerfilesController.cs
@@ -0,0 +1,20 @@
+﻿using DA.Models;
+using DA.Providers;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Api_Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PerfilesController
+    {
+        [HttpGet("getPerfiles")]
+        public async Task<string> getPerfiles()
+        {
+            TestProvider perfiles = new TestProvider();
+            IEnumerable<UserProfile> resultado = await perfiles.LeerPerfiles();
+            return JsonConvert.SerializeObject(resultado);
+        }
+    }
+}
---
+++ DA/Providers/TestProvider.cs
@@ -0,0 +1,28 @@
+﻿using DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.Providers
+{
+    public class TestProvider
+    {
+        public async Task<IEnumerable<UserProfile>> LeerPerfiles()
+        {
+            try
+            {
+                using (BD_Entities context = new BD_Entities())
+                {
+                    return await context.UserProfile.ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
